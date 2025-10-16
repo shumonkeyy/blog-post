be054583c23383f164bfab1245e2cc91fece824a
shumonkeyy <smmoepphyu@gmail.com>
2025-10-15 21:02:43 -0700

    vite config modified

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://blog-api-eight-delta.vercel.app/",
+        target: "https://blog-api-eight-delta.vercel.app",
         changeOrigin: true,
         // optional: rewrite /api -> /
         rewrite: (path) => path.replace(/^\/api/, ""),
